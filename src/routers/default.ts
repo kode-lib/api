49b7314b2e8b746729f5b99a3e6e52ed74873002
Tudor <tudor@marghidanu.com>
2021-03-22 11:55:13 +0200

    Health check  endpoint

--- src/routers/default.ts
+++ src/routers/default.ts
@@ -2,9 +2,15 @@ import { Router, Request, Response } from "express";
 
 export const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
+router.get("/", (_: Request, res: Response) => {
     res.json({
         name: process.env.npm_package_name,
         version: process.env.npm_package_version,
+    });
+});
+
+router.get("/health", (_: Request, res: Response) => {
+    res.json({
+        status: "OK"
     });
 });
